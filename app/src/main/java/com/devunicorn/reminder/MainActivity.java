8dfc63e62f8961230a6da62d7796f21b999edf81
Popkov Anton <popkow.anton@gmail.com>
2017-04-25 18:47:37 +0700

    Added Toolbar and search button

--- app/src/main/java/com/devunicorn/reminder/MainActivity.java
+++ app/src/main/java/com/devunicorn/reminder/MainActivity.java
@@ -3,15 +3,35 @@ package com.devunicorn.reminder;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 /**
  * Created by Dell on 25.04.2017.
  */
 
 public class MainActivity extends Activity {
+
+    private Toolbar toolBar;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        toolBar = (Toolbar) findViewById(R.id.toolbar);
+        toolBar.setTitle(R.string.app_name);
+        toolBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener(){
+            @Override
+                    public boolean onMenuItemClick(MenuItem menuItem){
+                return false;
+            }
+        });
+
+        toolBar.inflateMenu(R.menu.menu);
     }
 }
