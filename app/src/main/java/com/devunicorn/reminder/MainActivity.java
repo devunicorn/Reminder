a90c7d43978ce22472075cb92999e340d1fc2b9c
Popkov Anton <popkow.anton@gmail.com>
2017-04-26 17:57:11 +0700

    NavigationView plus one item, header

--- app/src/main/java/com/devunicorn/reminder/MainActivity.java
+++ app/src/main/java/com/devunicorn/reminder/MainActivity.java
@@ -2,6 +2,7 @@ package com.devunicorn.reminder;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
@@ -12,27 +13,35 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int LAYOUT = R.layout.activity_main;
+
     private Toolbar toolBar;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
+        setContentView(LAYOUT);
+
         initToolbar();
+        initNavigationView();
     }
 
     private void initToolbar() {
         toolBar = (Toolbar) findViewById(R.id.toolbar);
         toolBar.setTitle(R.string.app_name);
-        toolBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener(){
+        toolBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
             @Override
-                    public boolean onMenuItemClick(MenuItem menuItem){
+            public boolean onMenuItemClick(MenuItem menuItem) {
                 return false;
             }
         });
 
         toolBar.inflateMenu(R.menu.menu);
+    }
+
+    private void initNavigationView() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
     }
 }
